ordpool-space/ordpool

commit 40ad1ebbfa6541d4c5c7b73125252bf5e5bb118f

Adding mini-graphs on dashboard.

frontend/src/app/services/websocket.service.ts
@@ -216,6 +216,9 @@ export class WebsocketService {
   }
 
   want(data: string[]) {
+    if (data === this.lastWant) {
+      return;
+    }
     this.websocketSubject.next({action: 'want', data: data});
     this.lastWant = data;
   }

backend/src/routes.ts
@@ -15,8 +15,8 @@ class Routes {
 
   constructor() {
     if (!config.DB_DISABLED) {
-      this.createCache();
-      setInterval(this.createCache.bind(this), 600000);
+      // this.createCache();
+      // setInterval(this.createCache.bind(this), 600000);
     }
   }
 

frontend/src/app/app.module.ts
@@ -43,7 +43,7 @@ import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { FeesBoxComponent } from './components/fees-box/fees-box.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faChartArea, faCube, faDatabase, faInfo, faInfoCircle, faList, faQuestion, faQuestionCircle, faSearch, faTachometerAlt, faThList, faTv } from '@fortawesome/free-solid-svg-icons';
+import { faChartArea, faCube, faCubes, faDatabase, faInfo, faInfoCircle, faList, faQuestion, faQuestionCircle, faSearch, faTachometerAlt, faThList, faTv } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -101,7 +101,8 @@ export class AppModule {
     library.addIcons(faInfoCircle);
     library.addIcons(faChartArea);
     library.addIcons(faTv);
-    library.addIcons(faCube);
+    library.addIcons(faTachometerAlt);
+    library.addIcons(faCubes);
     library.addIcons(faThList);
     library.addIcons(faList);
     library.addIcons(faTachometerAlt);

frontend/src/app/components/block/block.component.ts
@@ -8,6 +8,7 @@ import { of, Subscription } from 'rxjs';
 import { StateService } from '../../services/state.service';
 import { SeoService } from 'src/app/services/seo.service';
 import { env } from 'src/app/app.constants';
+import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'app-block',
@@ -39,9 +40,11 @@ export class BlockComponent implements OnInit, OnDestroy {
     private electrsApiService: ElectrsApiService,
     private stateService: StateService,
     private seoService: SeoService,
+    private websocketService: WebsocketService,
   ) { }
 
   ngOnInit() {
+    this.websocketService.want(['blocks', 'mempool-blocks']);
     this.paginationMaxSize = window.matchMedia('(max-width: 700px)').matches ? 3 : 5;
     this.network = this.stateService.network;
 

frontend/src/app/components/master-page/master-page.component.html
@@ -29,7 +29,7 @@
           <a class="nav-link" [routerLink]="['/bisq']" (click)="collapse()"><fa-icon [icon]="['fas', 'list']" [fixedWidth]="true" title="Transactions"></fa-icon></a>
         </li>
         <li class="nav-item" routerLinkActive="active">
-          <a class="nav-link" [routerLink]="['/bisq/blocks']" (click)="collapse()"><fa-icon [icon]="['fas', 'th-list']" [fixedWidth]="true" title="Blocks"></fa-icon></a>
+          <a class="nav-link" [routerLink]="['/bisq/blocks']" (click)="collapse()"><fa-icon [icon]="['fas', 'cubes']" [fixedWidth]="true" title="Blocks"></fa-icon></a>
         </li>
         <li class="nav-item" routerLinkActive="active">
           <a class="nav-link" [routerLink]="['/bisq/stats']" (click)="collapse()"><fa-icon [icon]="['fas', 'tachometer-alt']" [fixedWidth]="true" title="Stats"></fa-icon></a>
@@ -37,10 +37,10 @@
       </ng-template>
       <ng-template #notBisq>
         <li class="nav-item" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">
-          <a class="nav-link" [routerLink]="['/' | relativeUrl]" (click)="collapse()"><fa-icon [icon]="['fas', 'cube']" [fixedWidth]="true" title="Dashboard"></fa-icon></a>
+          <a class="nav-link" [routerLink]="['/' | relativeUrl]" (click)="collapse()"><fa-icon [icon]="['fas', 'tachometer-alt']" [fixedWidth]="true" title="Dashboard"></fa-icon></a>
         </li>
         <li class="nav-item" routerLinkActive="active">
-          <a class="nav-link" [routerLink]="['/blocks' | relativeUrl]" (click)="collapse()"><fa-icon [icon]="['fas', 'th-list']" [fixedWidth]="true" title="Blocks"></fa-icon></a>
+          <a class="nav-link" [routerLink]="['/blocks' | relativeUrl]" (click)="collapse()"><fa-icon [icon]="['fas', 'cubes']" [fixedWidth]="true" title="Blocks"></fa-icon></a>
         </li>
         <li class="nav-item" routerLinkActive="active">
           <a class="nav-link" [routerLink]="['/graphs' | relativeUrl]" (click)="collapse()"><fa-icon [icon]="['fas', 'chart-area']" [fixedWidth]="true" title="Graphs"></fa-icon></a>

frontend/src/app/components/mempool-block/mempool-block.component.ts
@@ -6,6 +6,7 @@ import { MempoolBlock } from 'src/app/interfaces/websocket.interface';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { SeoService } from 'src/app/services/seo.service';
 import { env } from 'src/app/app.constants';
+import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'app-mempool-block',
@@ -23,9 +24,12 @@ export class MempoolBlockComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private stateService: StateService,
     private seoService: SeoService,
+    private websocketService: WebsocketService,
   ) { }
 
   ngOnInit(): void {
+    this.websocketService.want(['blocks', 'mempool-blocks']);
+
     this.mempoolBlock$ = this.route.paramMap
       .pipe(
         switchMap((params: ParamMap) => {

frontend/src/app/components/mempool-graph/mempool-graph.component.ts
@@ -13,6 +13,9 @@ import { StateService } from 'src/app/services/state.service';
 export class MempoolGraphComponent implements OnInit, OnChanges {
   @Input() data;
   @Input() dateSpan = '2h';
+  @Input() showLegend = true;
+  @Input() offsetX = 40;
+  @Input() offsetY = 40;
 
   mempoolVsizeFeesOptions: any;
   mempoolVsizeFeesData: any;
@@ -26,6 +29,9 @@ export class MempoolGraphComponent implements OnInit, OnChanges {
   ) { }
 
   ngOnInit(): void {
+    const showLegend = !this.isMobile && this.showLegend;
+    const labelHops = this.isMobile || !this.showLegend ? 12 : 6;
+
     const labelInterpolationFnc = (value: any, index: any) => {
       switch (this.dateSpan) {
         case '2h':
@@ -41,7 +47,7 @@ export class MempoolGraphComponent implements OnInit, OnChanges {
         case '1y':
           value = formatDate(value, 'dd/MM', this.locale);
       }
-      return index % 6  === 0 ? value : null;
+      return index % labelHops  === 0 ? value : null;
     };
 
     this.mempoolVsizeFeesOptions = {
@@ -52,23 +58,28 @@ export class MempoolGraphComponent implements OnInit, OnChanges {
       low: 0,
       axisX: {
         labelInterpolationFnc: labelInterpolationFnc,
-        offset: 40
+        offset: this.offsetX,
       },
       axisY: {
         labelInterpolationFnc: (value: number): any => {
           return this.vbytesPipe.transform(value, 2);
         },
-        offset: this.isMobile ? 60 : 160
+        offset: showLegend ? 160 : 60,
       },
       plugins: [
         Chartist.plugins.ctTargetLine({
           value: 1000000
         }),
+      ]
+    };
+
+    if (showLegend) {
+      this.mempoolVsizeFeesOptions.plugins.push(
         Chartist.plugins.legend({
           legendNames: [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 175, 200,
             250, 300, 350, 400].map((sat, i, arr) => {
               if (sat === 400) {
-               return '350+';
+              return '350+';
               }
               if (i === 0) {
                 if (this.stateService.network === 'liquid') {
@@ -79,8 +90,8 @@ export class MempoolGraphComponent implements OnInit, OnChanges {
               return arr[i - 1] + ' - ' + sat;
             })
         })
-      ]
-    };
+      );
+    }
   }
 
   ngOnChanges() {

frontend/src/app/components/start/start.component.ts
@@ -1,17 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { WebsocketService } from 'src/app/services/websocket.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss'],
 })
-export class StartComponent implements OnInit {
-  constructor(
-    private websocketService: WebsocketService,
-  ) { }
-
-  ngOnInit() {
-    this.websocketService.want(['blocks', 'stats', 'mempool-blocks']);
-  }
+export class StartComponent {
+  constructor() { }
 }

commit d984a4360e5b67bf72dd190196b2e27062028856

Bugfix: Websocket data push stopped working on disconnect, bug caused by 40ad1ebb

frontend/src/app/services/websocket.service.ts
@@ -158,7 +158,7 @@ export class WebsocketService {
         if (this.goneOffline === true) {
           this.goneOffline = false;
           if (this.lastWant) {
-            this.want(this.lastWant);
+            this.want(this.lastWant, true);
           }
           this.stateService.connectionState$.next(2);
         }
@@ -217,8 +217,8 @@ export class WebsocketService {
     this.websocketSubject.next({ historicalDate });
   }
 
-  want(data: string[]) {
-    if (data === this.lastWant) {
+  want(data: string[], force = false) {
+    if (data === this.lastWant && !force) {
       return;
     }
     this.websocketSubject.next({action: 'want', data: data});