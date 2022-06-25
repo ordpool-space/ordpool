ordpool-space/ordpool

commit 9626311aca09a98c9349278cee8289a8f381aef9

Effective fee could go below 0 due to rounding error. (#623)
fixes #606

backend/src/api/common.ts
@@ -105,7 +105,7 @@ export class Common {
       totalFees += tx.bestDescendant.fee;
     }
 
-    tx.effectiveFeePerVsize = totalFees / (totalWeight / 4);
+    tx.effectiveFeePerVsize = Math.max(1, totalFees / (totalWeight / 4));
     tx.cpfpChecked = true;
 
     return {

commit bea00ea2dc436678eaa3bd75eee66a63e04c8ab1

Fixes the minium tx fee bug introduced in 9626311aca09a98c9349278cee8289a8f381aef9

backend/src/api/common.ts
@@ -114,7 +114,7 @@ export class Common {
       totalFees += tx.bestDescendant.fee;
     }
 
-    tx.effectiveFeePerVsize = Math.max(Common.isLiquid() ? 0.1 : 1, totalFees / (totalWeight / 4));
+    tx.effectiveFeePerVsize = Math.max(0, totalFees / (totalWeight / 4));
     tx.cpfpChecked = true;
 
     return {