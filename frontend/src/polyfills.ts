19ded528c5416c8d226b8de160c8d4d9070dbc40
Hans Crypto <johannes@haushoppe.art>
2024-02-03 00:57:46 +0100

    first working browser version of ECPairFactory

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -57,3 +57,7 @@ import 'zone.js';  // Included with Angular CLI.
  */
 
 (window as any).global = window;
+
+
+// required for the ECC library
+window.Buffer = window.Buffer || require('buffer').Buffer;
