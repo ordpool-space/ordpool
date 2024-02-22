0f45cb1565986e726b27087e4d3a5878a74f1682
Hans Crypto <johannes@haushoppe.art>
2024-02-22 23:30:15 +0100

    let's see if it somehow works

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,10 +2,17 @@ export const environment = {
   production: true,
   nativeAssetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
   nativeTestAssetId: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
-  // looks like they blocked us 😞
+
+  // blocked on main domain!
   // apiBaseUrl: 'https://mempool.space',
   // websocketBaseUrl: 'wss://mempool.space',
-  apiBaseUrl: 'https://mempool.nixbitcoin.org',
-  websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
+
+  // tmp. fallback
+  // apiBaseUrl: 'https://mempool.nixbitcoin.org',
+  // websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
+
+  apiBaseUrl: 'https://api.ordinalsbot.com/mempool',
+  websocketBaseUrl: 'wss://api.ordinalsbot.com/mempool',
+
   enableInscriptionAccelerator: true
 };
