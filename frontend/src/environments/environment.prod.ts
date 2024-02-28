e73f6be2f86294b1e4c87671ca951d0beb752704
Hans Crypto <johannes@haushoppe.art>
2024-02-28 09:48:44 +0100

    Revert "let's see if it somehow works"
    This reverts commit 0f45cb1565986e726b27087e4d3a5878a74f1682.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,17 +2,10 @@ export const environment = {
   production: true,
   nativeAssetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
   nativeTestAssetId: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
-
-  // blocked on main domain!
+  // looks like they blocked us 😞
   // apiBaseUrl: 'https://mempool.space',
   // websocketBaseUrl: 'wss://mempool.space',
-
-  // tmp. fallback
-  // apiBaseUrl: 'https://mempool.nixbitcoin.org',
-  // websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
-
-  apiBaseUrl: 'https://api.ordinalsbot.com/mempool',
-  websocketBaseUrl: 'wss://api.ordinalsbot.com/mempool',
-
+  apiBaseUrl: 'https://mempool.nixbitcoin.org',
+  websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
   enableInscriptionAccelerator: true
 };
