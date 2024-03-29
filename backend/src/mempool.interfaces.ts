Brandylee24/mempool

Use minfee node to limit gbt input size

backend/src/mempool.interfaces.ts
@@ -96,6 +96,12 @@ export interface AuditTransaction {
   modifiedNode: HeapNode<AuditTransaction>;
 }
 
+export interface GbtCandidates {
+  txs: { [txid: string ]: boolean },
+  added: string[];
+  removed: string[];
+}
+
 export interface ThreadTransaction {
   txid: string;
   fee: number;

ordpool-space/ordpool

backend/src/mempool.interfaces.ts
@@ -143,6 +143,12 @@ export interface CompactThreadTransaction {
   dirty?: boolean;
 }
 
+export interface GbtCandidates {
+  txs: { [txid: string ]: boolean },
+  added: MempoolTransactionExtended[];
+  removed: MempoolTransactionExtended[];
+}
+
 export interface ThreadTransaction {
   txid: string;
   fee: number;