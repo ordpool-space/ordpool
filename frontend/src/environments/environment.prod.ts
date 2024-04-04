8fa1cd4fa8d97a7767868cb7dd3eb7c509f5f737
Hans Crypto <johannes@haushoppe.art>
2024-04-05 00:17:35 +0200

    btc.SigHash.ALL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,13 +2,12 @@ export const environment = {
   production: true,
   nativeAssetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
   nativeTestAssetId: '144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49',
-  // looks like they blocked us 😞
-  // apiBaseUrl: 'https://mempool.space',
-  // websocketBaseUrl: 'wss://mempool.space',
-  apiBaseUrl: 'https://mempool.nixbitcoin.org',
-  websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
+  // apiBaseUrl: 'https://mempool.nixbitcoin.org',
+  // websocketBaseUrl: 'wss://mempool.nixbitcoin.org',
+  apiBaseUrl: 'https://mempool.space',
+  websocketBaseUrl: 'wss://mempool.space',
   enableInscriptionAccelerator: true,
-  enableCat21Mint: false,
+  enableCat21Mint: true,
   ordBaseUrl: 'https://explorer.ordinalsbot.com',
   ordBaseUrlTestnet: 'https://testnet-explorer.ordinalsbot.com',
   cat21BaseUrl: 'https://backend.cat21.space'
