4f3e0ee9e3f952fd48ed2af2be540e32ff0ca08c
Hans Crypto <johannes@haushoppe.art>
2024-05-20 23:29:54 +0200

    removing minor differences (like white spaces)

--- frontend/src/app/services/electrs-api.service.ts
+++ frontend/src/app/services/electrs-api.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, Injector, forwardRef } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { BehaviorSubject, Observable, catchError, filter, from, of, shareReplay, switchMap, take, tap } from 'rxjs';
 import { Transaction, Address, Outspend, Recent, Asset, ScriptHash, AddressTxSummary } from '../interfaces/electrs.interface';
@@ -6,7 +6,6 @@ import { StateService } from './state.service';
 import { BlockExtended } from '../interfaces/node-api.interface';
 import { calcScriptHash$ } from '../bitcoin.utils';
 import { environment } from 'src/environments/environment';
-import { DigitalArtifactsFetcherService } from './ordinals/digital-artifacts-fetcher.service';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +19,7 @@ export class ElectrsApiService {
   constructor(
     private httpClient: HttpClient,
     private stateService: StateService,
-    private injector: Injector) {
+   ) {
     // HACK
     // this.apiBaseUrl = ''; // use relative URL by default
     this.apiBaseUrl = environment.apiBaseUrl;
