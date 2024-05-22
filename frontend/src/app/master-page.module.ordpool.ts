15e96f5b47a52b16daf3761ecd7eb3efd639e1fd
Hans Crypto <johannes@haushoppe.art>
2024-05-22 02:17:50 +0200

    re-implemention of the original orange ordpool design

---
+++ frontend/src/app/master-page.module.ordpool.ts
@@ -0,0 +1,19 @@
+
+import { Cat21MintComponent } from './components/ordinals/cat21-mint/cat21-mint.component';
+import { Cat21CollabComponent } from './components/ordinals/cat21-collab/cat21-collab.component';
+import { Cat21WhitelistCheckerComponent } from './components/ordinals/cat21-whitelist-checker/cat21-whitelist-checker.component';
+
+export const extraOrdpoolRoutes = [
+  {
+  path: 'cat21-mint',
+  component: Cat21MintComponent,
+  },
+  {
+    path: 'cat21-collab',
+    component: Cat21CollabComponent,
+  },
+  {
+    path: 'cat21-whitelist-checker',
+    component: Cat21WhitelistCheckerComponent,
+  }
+];
