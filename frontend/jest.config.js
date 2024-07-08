331a2fdb5e583b8f5affca95aa34c759824778c7
Hans Crypto <johannes@haushoppe.art>
2024-07-08 11:45:23 +0200

    testing: using jest-preset-angular for better tooling

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,16 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  // this test emulates a browser environment
-  testEnvironment: 'jsdom',
-  testMatch: ['**/*.jest.ts'],
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
-  transform: {
-    '^.+\\.ts$': [
-      'ts-jest', {
-        tsconfig: '<rootDir>/tsconfig.app.json',
-        diagnostics: { ignoreCodes: ['TS151001'] }
-      }
-    ],
-  },
+  preset: 'jest-preset-angular',
+  // testMatch: ['**/*.jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/cypress/'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
 };
